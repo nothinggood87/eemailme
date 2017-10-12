e70656ea92766403fd93c2ea7c79abc6b1436d3a
Jacob Hands <jacob@gogit.io>
2017-10-12 11:22:10 -0500

    Add Convert-MboxToTextFiles

--- EEMailMe.psm1
+++ EEMailMe.psm1
@@ -37,26 +37,58 @@ Function Get-MboxMessage {
         $r = [IO.File]::OpenText($Path)
         while ($r.Peek() -ge 0) {
             $line = $r.ReadLine()
-            
-            # if ($count -gt 557) {
-            #     Write-Verbose $line
-            # }
-            if ($line -match 'From [0-9]+@xxx [a-zA-Z]{3} [a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \+0000 [0-9]{4}') {
-                $count++
-                $pipe = $msg
-                $msg = [MboxMessage]::new()
-                if ($first) {
-                    $first = $false
-                } else {
-                    $pipe
+            if($line.StartsWith('From')){
+                if ($line -match 'From [0-9]+@xxx [a-zA-Z]{3} [a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \+0000 [0-9]{4}') {
+                    $count++
+                    $pipe = $msg
+                    $msg = [MboxMessage]::new()
+                    if ($first) {
+                        $first = $false
+                    } else {
+                        $pipe
+                    }
                 }
             }
+            
             $msg.Content += $line
         }
         $r.Dispose()
     }
     End {
         
+    }
+}
+Function Convert-MboxToTextFiles {
+    <#
+    .SYNOPSIS
+    Splits up a .mbox file to .txt files for each message
+    
+    .DESCRIPTION
+    Splits up a .mbox file to .txt files for each message
+    
+    .PARAMETER MboxPath
+    Path to .mbox file
+    
+    .PARAMETER OutputPath
+    Directory to place message .txt files
+    
+    .EXAMPLE
+     Convert-MboxToTextFiles -MboxPath $path -OutputPath $out
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateScript( {Test-Path $_ -PathType Leaf} )]
+        [string]$MboxPath,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript( {Test-Path $_ -PathType Container} )]
+        [string]$OutputPath
+    )
+    $id = 0
+    Get-MboxMessage $MboxPath | ForEach-Object {
+        $file = ($OutputPath+"\message_$id.txt")
+        $_.Content | Out-File $file
+        $id++
     }
 }
 
