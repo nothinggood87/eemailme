25ee87474a27667c0e8716a8903fda213b83b509
Jacob Hands <jacob@gogit.io>
2017-11-07 09:08:49 -0600

    Add support for more computer names.

--- GmailTools/GmailTools/Config.cs
+++ GmailTools/GmailTools/Config.cs
@@ -8,8 +8,45 @@ namespace GmailTools
 {
     class Config
     {
-        public static string ProjectPath => "C:\\Dropbox\\Projects\\eemailme";
-        public static string ReportsPath => $"{ProjectPath}\\Reports\\Jacob";
+        private static List<string> andrewPCs = new List<string>
+        {
+            "ANDREW-LAPTOP",
+            "ANDREW-DESKTOP"
+        };
+        private static List<string> jacobPCs = new List<string>
+        {
+            "ENVY10",
+            "DT10",
+            "DTGAME",
+            "ENVY13"
+        };
+        public static string User
+        {
+            get
+            {
+                string pc = Environment.MachineName;
+                if (jacobPCs.Contains(pc)) { return "Jacob"; }
+                else if (andrewPCs.Contains(pc)) { return "Andrew"; }
+                else { throw new Exception("Your computer name is not supported!"); }
+            }
+        }
+        public static string ProjectPath
+        {
+            get
+            {
+                switch (Environment.MachineName)
+                {
+                    // Jacob
+                    case "ENVY10": return "C:\\Dropbox\\Projects\\eemailme";
+                    case "DT10": return "V:\\Dropbox\\Projects\\eemailme";
+                    // Andrew
+                    case "ANDREW-LAPTOP": return "C:\\Users\\andre\\Dropbox\\eemailme";
+                    case "ANDREW-DESKTOP": return "C:\\Users\\andre\\Dropbox\\eemailme";
+                }
+                throw new Exception("Your computer name is not supported!");
+            }
+        }
+        public static string ReportsPath => $"{ProjectPath}\\Reports\\{User}";
         public static string DataPath => $"{ProjectPath}\\Data";
         public static List<string> GmailSystemLabels => new List<string>
         {
@@ -20,7 +57,7 @@ namespace GmailTools
             "STARRED",
             "IMPORTANT",
             "SENT",
-            "DRAFT",
+            "DRAFTS",
             "CATEGORY_PERSONAL",
             "CATEGORY_SOCIAL",
             "CATEGORY_PROMOTIONS",
